ed651893eb01eeef78e8ea0131e731eedae060b1
m-kawabt <cfilorvy234567890@gmail.com>
2023-03-14 10:27:25 +0900

    edit

--- data/fetch_data_from_bitflyer.py
+++ data/fetch_data_from_bitflyer.py
@@ -1,22 +1,35 @@
 import asyncio
 import json
 import aiohttp
-from time import sleep
 
 
-async def get_data():
-    uri = 'wss://ws.lightstream.bitflyer.com/json-rpc'
-    query = {'method': 'subscribe', 'params': {'channel': 'lightning_ticker_FX_BTC_JPY'}}
-    while True:
-        async with aiohttp.ClientSession() as session:
-            async with session.ws_connect(uri, receive_timeout=60) as client:
-                await asyncio.wait([client.send_str(json.dumps(query))])
-                async for response in client:
-                    if response.type != aiohttp.WSMsgType.TEXT:
-                        print('response: ' + str(response))
-                        break
-                    data = json.loads(response[1])
-                    print(data)
+class chart_1m:
+    data_list = []
 
-loop = asyncio.get_event_loop()
-loop.run_until_complete(get_data())
+    def __init__(self):
+        loop = asyncio.get_event_loop()
+        tasks = asyncio.wait([self.get_data(), self.test()])
+        loop.run_until_complete(tasks)
+
+    async def get_data(self):
+        uri = 'wss://ws.lightstream.bitflyer.com/json-rpc'
+        query = {'method': 'subscribe', 'params': {'channel': 'lightning_ticker_FX_BTC_JPY'}}
+        while True:
+            async with aiohttp.ClientSession() as session:
+                async with session.ws_connect(uri, receive_timeout=60) as client:
+                    await asyncio.wait([client.send_str(json.dumps(query))])
+                    async for response in client:
+                        if response.type != aiohttp.WSMsgType.TEXT:
+                            print('response: ' + str(response))
+                            break
+                        data = json.loads(response[1])['params']['message']
+                        # print(data)
+                        self.data_list.append(data)
+                    print('a')
+
+    async def test(self):
+        while True:
+            if self.data_list:
+                print(self.data_list)
+                self.data_list = []
+                await asyncio.sleep(3)
