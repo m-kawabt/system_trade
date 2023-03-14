0a4df22f9550bb5f47292639b81756fd874cfccc
m-kawabt <cfilorvy234567890@gmail.com>
2023-03-14 09:49:52 +0900

    edit

--- data/fetch_data_from_bitflyer.py
+++ data/fetch_data_from_bitflyer.py
@@ -1,1 +1,22 @@
+import asyncio
+import json
+import aiohttp
+from time import sleep
 
+
+async def get_data():
+    uri = 'wss://ws.lightstream.bitflyer.com/json-rpc'
+    query = {'method': 'subscribe', 'params': {'channel': 'lightning_ticker_FX_BTC_JPY'}}
+    while True:
+        async with aiohttp.ClientSession() as session:
+            async with session.ws_connect(uri, receive_timeout=60) as client:
+                await asyncio.wait([client.send_str(json.dumps(query))])
+                async for response in client:
+                    if response.type != aiohttp.WSMsgType.TEXT:
+                        print('response: ' + str(response))
+                        break
+                    data = json.loads(response[1])
+                    print(data)
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(get_data())
