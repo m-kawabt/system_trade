006f9980ae2470820e7a410225a446b3617479be
m-kawabt <cfilorvy234567890@gmail.com>
2023-03-16 18:39:46 +0900

    edit

--- data/fetch_data_from_cryptowatch.py
+++
@@ -1,7 +0,0 @@
-import requests
-import mplfinance
-
-response = requests.get("https://api.cryptowat.ch/markets/bitflyer/btcfxjpy/ohlc?periods=60")
-response = response.json()
-
-print(response["result"]["60"])
